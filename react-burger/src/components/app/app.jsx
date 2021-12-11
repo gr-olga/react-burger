062cbcac0f7912079d4cda3f901a28b2e07d60a0
Olga Panfilova <gr-olga@ya.ru>
2021-12-11 18:49:36 +0100

    add api

--- react-burger/src/components/app/app.jsx
+++ react-burger/src/components/app/app.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './app.module.css';
 import AppHeader from "../header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import {data} from "../utils/data";
+
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 
 function App() {
     const baseUrl = "https://norma.nomoreparties.space/api/ingredients"
+    const [ingredients, setIngredients] = useState([])
 
-    function getIngredientsData(baseUrl){
-        return fetch(baseUrl,{
-
-        })
+    function getIngredientsData(baseUrl) {
+        return fetch(baseUrl)
+            .then((res) => {
+                return (res.json());
+            })
+            .then(({data}) => {
+                setIngredients(data);
+            })
+            .catch((err)=>{
+                console.log("failed",err);
+            })
     }
+
+    useEffect(() => {
+        getIngredientsData(baseUrl)
+    },[])
 
     return (
         <div className={styles.app}>
             <AppHeader/>
             <div className={styles.bar}>
-                <BurgerIngredients data={data}/>
-                <BurgerConstructor data={data}/>
+                <BurgerIngredients data={ingredients}/>
+                <BurgerConstructor data={ingredients}/>
             </div>
         </div>
     );
