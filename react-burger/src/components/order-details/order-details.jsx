5e95f957e6e0a99e110ce1ea46a4a57cce089274
Olga Panfilova <gr-olga@ya.ru>
2021-12-13 10:24:19 +0100

    css overlay order-details

--- react-burger/src/components/order-details/order-details.jsx
+++ react-burger/src/components/order-details/order-details.jsx
@@ -5,13 +5,11 @@ import styles from './order-ditails.module.css'
 function OrderDetails(){
     return(
         <ModalOverlay>
-            <div className={styles.container}>
             <p className="text text_type_digits-large">123456</p>
             <h2 className={styles.title}>идентификатор заказа</h2>
             <img src={CekMark} className={styles.image}/>
             <h3 className={styles.orderStatus}>Ваш заказ начали готовить</h3>
             <h3 className={styles.addInfo}>Дождитесь готовности на орбитальной станции</h3>
-        </div>
         </ModalOverlay>
     )
 }
