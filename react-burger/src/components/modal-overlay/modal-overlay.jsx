7d32e6eac0b82bc6b97713c059c118c99d33c542
Olga Panfilova <gr-olga@ya.ru>
2021-12-17 15:07:21 +0100

    fix after revue3 add ESC close

--- react-burger/src/components/modal-overlay/modal-overlay.jsx
+++ react-burger/src/components/modal-overlay/modal-overlay.jsx
@@ -1,30 +1,28 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect} from "react";
 import styles from './modal-overlay.module.css'
 
 
 function ModalOverlay(props) {
-    const ESCAPE_BTN = 27;
 
-    function closeModalKey(evt) {
-        if (evt.keyCode === ESCAPE_BTN) props.onClose();
-    }
+    useEffect(() => {
+        const closeByEscape = (e) => {
+            if (e.key === "Escape") {
+                props.onClose()
+            }
+        }
+        document.addEventListener('keydown', closeByEscape)
+
+        return () => document.removeEventListener('keydown', closeByEscape)
+    }, [])
+
 
     function closeModalClick(evt) {
         if (evt.target.classList.contains(styles.overlay)) props.onClose()
     }
 
-    const overlayRef = useRef(null);
-
-    useEffect(() => {
-        overlayRef.current.focus();
-    })
-
     return (
-        <div className={props.isOpen ? `${styles.modal_open} ${styles.overlay}` : styles.overlay }
-             tabIndex="0"
-             onKeyDown={closeModalKey}
+        <div className={props.isOpen ? `${styles.modal_open} ${styles.overlay}` : styles.overlay}
              onClick={closeModalClick}
-             ref={overlayRef}
         >
             {props.children}
         </div>
