8fbb43af0d7348a40bc2638b7e7ac5670dd7db52
Olga Panfilova <gr-olga@ya.ru>
2022-03-12 19:03:26 +0100

    fixes order

--- react-burger/src/components/burger-constructor/burger-constructor.tsx
+++ react-burger/src/components/burger-constructor/burger-constructor.tsx
@@ -7,8 +7,12 @@ import {useDrop} from "react-dnd";
 import bun from '../../images/bun01.png'
 import {ADD_INGREDIENT_TO_NON_BUN_ITEMS, getOrderIngredients} from "../../services/actions";
 import ConstructorItem from "../constructor-item/constructor-item";
+import {getCookie} from "../../utils/cookies-helpers";
+import {useHistory} from "react-router-dom";
+
 
 function BurgerConstructor(props: BurgerConstructorTypes) {
+  const history = useHistory()
     const {
         constructorIngredients,
         nonBunIngredientsList,
@@ -38,6 +42,11 @@ function BurgerConstructor(props: BurgerConstructorTypes) {
     });
 
     function handleOrderDetailClick() {
+        const token = getCookie('accessToken');
+        if (!token) {
+           history.push( "/login")
+        }
+        else
         dispatch(getOrderIngredients(constructorIngredients.map((item) => item._id)))
     }
 
